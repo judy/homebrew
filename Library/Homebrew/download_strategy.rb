6ffd472425cf999c3a42806cbc29a8ae4beb8d4d
Adam Vandenberg <flangy@gmail.com>
2011-03-12 16:56:33 -0800

    wip

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -247,11 +247,15 @@ class GitDownloadStrategy <AbstractDownloadStrategy
   end
 
   def url_to_use
-    if @url.startswith("git://github.com")
-      return smart-http-version
-    else
-      return @url
+    # If the user requests, translate GitHub git URLs into
+    # https URLs, to work around firewalls.
+    if ENV['HOMEBREW_SMART_HTTP']
+      if @url =~ %r{git://github.com/(.+.git)}
+        return "https://github.com/"+$1
+      end
     end
+
+    return @url
   end
 
   def cached_location
@@ -263,7 +267,7 @@ class GitDownloadStrategy <AbstractDownloadStrategy
           "  brew install git\n" \
           unless system "/usr/bin/which git"
 
-    ohai "Cloning #{@url_to_use}"
+    ohai "Cloning #{url_to_use}"
 
     if @clone.exist?
       Dir.chdir(@clone) do
@@ -276,11 +280,11 @@ class GitDownloadStrategy <AbstractDownloadStrategy
     end
 
     unless @clone.exist?
-      safe_system 'git', 'clone', '--depth', '1', @url_to_use, @clone # indeed, leave it verbose
+      safe_system 'git', 'clone', '--depth', '1', url_to_use, @clone # indeed, leave it verbose
     else
       puts "Updating #{@clone}"
       Dir.chdir(@clone) do
-        quiet_safe_system 'git', 'fetch', @url_to_use
+        quiet_safe_system 'git', 'fetch', url_to_use
         # If we're going to checkout a tag, then we need to fetch new tags too.
         quiet_safe_system 'git', 'fetch', '--tags' if @spec == :tag
       end
